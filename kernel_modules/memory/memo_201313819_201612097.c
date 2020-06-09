4da8ce17be37c55f648fe5735dcd0c48df27b983
Ubuntu <ubuntu@ip-172-31-51-244.ec2.internal>
2020-06-09 19:01:49 +0000

    actuailizando modulo v3

--- kernel_modules/memory/memo_201313819_201612097.c
+++ kernel_modules/memory/memo_201313819_201612097.c
@@ -26,9 +26,9 @@ static int write_file(struct seq_file * archivo, void *v){
     seq_printf(archivo,"ERICK ALEXANDER LEMUS MORALES - 201612097\n");
     seq_printf(archivo,"PROYECTO 1 - MODULO DE MEMORIA\n");
     seq_printf(archivo,"\n");
-    seq_printf(archivo,"TOTAL MEMORIA -> %8lu kb - %8lu mb \n", memoriatotal, memoriatotal/1024);
+    seq_printf(archivo,"TOTAL MEMORIA -> \t  %8lu kb - %8lu mb \n", memoriatotal, memoriatotal/1024);
     seq_printf(archivo,"MEMORIA LIBRE -> \t %8lu KB - %8lu MB \n", memorialibre, memorialibre/1024);
-    seq_printf(archivo,"MEMORIA EN ESO -> \t %i %%\n", (memorialibre * 100)/memoriatotal);
+    seq_printf(archivo,"MEMORIA EN USO -> \t %i %%\n", (memorialibre * 100)/memoriatotal);
     seq_printf(archivo,"\n");
     return 0;
 }
@@ -45,14 +45,14 @@ static struct file_operations ops =
 };
 
 static int iniciar(void){
-    proc_create("memo_201313819,201612097",0, NULL,&ops);
-    printk(KERN_INFO "Carnet1: 201313819, Carnet2: 201612097");
+    proc_create("memo_201313819_201612097",0, NULL,&ops);
+    printk("\nCarnet1: 201313819, Carnet2: 201612097\n");
     return 0;
 }
 
 static void salir(void){
-    remove_proc_entry("salida",NULL);
-    printk(KERN_INFO "Sistemas Operativos 1");
+    remove_proc_entry("memo_201313819_201612097",NULL);
+    printk("\nSistemas Operativos 1\n");
 }
 
 module_init(iniciar);
