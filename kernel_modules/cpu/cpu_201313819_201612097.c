c6c310c37387b3f6d8f6a65b20244184b0e09fb1
erclem1998 <61994737+erclem1998@users.noreply.github.com>
2020-06-09 22:08:30 -0600

    actualizando modulo cpu v2

--- kernel_modules/cpu/cpu_201313819_201612097.c
+++ kernel_modules/cpu/cpu_201313819_201612097.c
@@ -14,7 +14,7 @@
 
 void recorrerHijos(struct seq_file *sf, struct task_struct *ts, int contador)
 {
-    struct task_struct *root = current;
+    struct task_struct *root,*task = current;
     while (root->pid != 1)
     {
         root = root->parent;
