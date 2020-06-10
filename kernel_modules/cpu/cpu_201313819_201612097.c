ebfbd1905c7650d0eddd5446e79a43b0aaaf6582
erclem1998 <61994737+erclem1998@users.noreply.github.com>
2020-06-09 22:01:17 -0600

    actualizando modulo cpu v2

--- kernel_modules/cpu/cpu_201313819_201612097.c
+++ kernel_modules/cpu/cpu_201313819_201612097.c
@@ -1,31 +1,57 @@
-#include <linux/module.h>       // Needed by all modules
-#include <linux/kernel.h>       // KERN_INFO
-//#include <linux/sched.h>        // for_each_process, pr_info
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/list.h>
+#include <linux/types.h>
+#include <linux/slab.h>
+#include <linux/sched.h>
+#include <linux/string.h>
+#include <linux/fs.h>
+#include <linux/seq_file.h>
+#include <linux/proc_fs.h>
+#include <linux/mm.h>
 #include <linux/sched/signal.h>
 
-void procs_info_print(void)
+void recorrerHijos(struct seq_file *sf, struct task_struct *ts, int contador)
 {
-        struct task_struct* task_list;
-        size_t process_counter = 0;
-        for_each_process(task_list) {
-                pr_info("== %s [%d]\n", task_list->comm, task_list->pid);
-                ++process_counter;
-        }
-        printk(KERN_INFO "== Number of process: %zu\n", process_counter);
+    struct task_struct *root = current;
+    while (root->pid != 1)
+    {
+        root = root->parent;
+    }
+    list_for_each(list, &ts->children)
+    {
+        task = list_entry(list, struct task_struct, sibling);
+        pr_info("== %s [%d]\n", root->comm, root->pid);
+        recorrerHijos(sf, task, contador + 1);
+        recorrerHijos(sf, root, 1);
+    }
 }
+
+/*void procs_info_print(void)
+{
+    struct task_struct *task_list;
+    size_t process_counter = 0;
+    for_each_process(task_list)
+    {
+        pr_info("== %s [%d]\n", task_list->comm, task_list->pid);
+        ++process_counter;
+    }
+    printk(KERN_INFO "== Number of process: %zu\n", process_counter);
+}*/
 
 int init_module(void)
 {
-        printk(KERN_INFO "[ INIT ==\n");
+    printk(KERN_INFO "[ INIT ==\n");
 
-        procs_info_print();
+    procs_info_print();
 
-        return 0;
+    return 0;
 }
 
 void cleanup_module(void)
 {
-        printk(KERN_INFO "== CLEANUP ]\n");
+    printk(KERN_INFO "== CLEANUP ]\n");
 }
 
 MODULE_LICENSE("MIT");
