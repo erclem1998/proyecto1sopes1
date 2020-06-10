b3675206a0f09645acbcb7bf34808330ca16b2ee
Ubuntu <ubuntu@ip-172-31-51-244.ec2.internal>
2020-06-10 03:31:09 +0000

    actualizando modulo cpu v2

--- kernel_modules/cpu/cpu_201313819_201612097.c
+++ kernel_modules/cpu/cpu_201313819_201612097.c
@@ -1,7 +1,7 @@
 #include <linux/module.h>       // Needed by all modules
 #include <linux/kernel.h>       // KERN_INFO
-#include <linux/sched.h>        // for_each_process, pr_info
-
+//#include <linux/sched.h>        // for_each_process, pr_info
+#include <linux/sched/signal.h>
 
 void procs_info_print(void)
 {
