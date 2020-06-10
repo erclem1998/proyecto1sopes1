958392c2a5f917470e82feb6f82e988417ce0e69
erclem1998 <61994737+erclem1998@users.noreply.github.com>
2020-06-09 23:12:01 -0600

    actualizando modulo cpu v2

--- kernel_modules/cpu/cpu_201313819_201612097.c
+++ kernel_modules/cpu/cpu_201313819_201612097.c
@@ -10,9 +10,9 @@
 #include <linux/seq_file.h>
 #include <linux/proc_fs.h>
 #include <linux/mm.h>
-//#include <linux/sched/signal.h>
+#include <linux/sched/signal.h>
 
-void recorrerHijos(struct seq_file *sf, struct task_struct *ts, int contador)
+/*void recorrerHijos(struct seq_file *sf, struct task_struct *ts, int contador)
 {
     struct list_head *list;
     struct task_struct *task;
@@ -23,32 +23,32 @@ void recorrerHijos(struct seq_file *sf, struct task_struct *ts, int contador)
         pr_info("Nombre: %s PID: %d Estado: %d\n", ts->comm, ts->pid, ts->state);
         recorrerHijos(sf, task, contador + 1);
     }
-}
+}*/
 
-/*void procs_info_print(void)
+void procs_info_print(void)
 {
     struct task_struct *task_list;
     size_t process_counter = 0;
     for_each_process(task_list)
     {
-        pr_info("== %s [%d]\n", task_list->comm, task_list->pid);
+        pr_info("Nombre: %s PID: %d  Estado: %d\n", task_list->comm, task_list->pid, task_list->state);
         ++process_counter;
     }
     printk(KERN_INFO "== Number of process: %zu\n", process_counter);
-}*/
+}
 
 int init_module(void)
 {
     printk(KERN_INFO "[ INIT ==\n");
-    struct task_struct *root = current;
+    /*struct task_struct *root = current;
     while (root->pid != 1)
     {
         root = root->parent;
     }
     struct seq_file *sf;
     recorrerHijos(sf, root, 1);
-
-    //procs_info_print();
+    */
+    procs_info_print();
 
     return 0;
 }
