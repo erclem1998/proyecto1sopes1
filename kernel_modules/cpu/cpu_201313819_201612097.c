31a38b7aebdc18ac19452a56f1b16eaf1d103aee
erclem1998 <61994737+erclem1998@users.noreply.github.com>
2020-06-11 17:20:03 -0600

    actualizando modulo cpu v2

--- kernel_modules/cpu/cpu_201313819_201612097.c
+++ kernel_modules/cpu/cpu_201313819_201612097.c
@@ -1,4 +1,4 @@
-#include <linux/init.h>
+/*#include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/list.h>
@@ -11,7 +11,7 @@
 #include <linux/proc_fs.h>
 #include <linux/mm.h>
 #include <linux/sched/signal.h>
-
+*/
 /*void recorrerHijos(struct seq_file *sf, struct task_struct *ts, int contador)
 {
     struct list_head *list;
@@ -24,7 +24,7 @@
         recorrerHijos(sf, task, contador + 1);
     }
 }*/
-
+/*
 
 typedef struct procesos
 {
@@ -50,7 +50,7 @@ void procs_info_print(void)
 
 int init_module(void)
 {
-    printk(KERN_INFO "[ INIT ==\n");
+    printk(KERN_INFO "[ INIT ==\n");*/
     /*struct task_struct *root = current;
     while (root->pid != 1)
     {
@@ -59,7 +59,7 @@ int init_module(void)
     struct seq_file *sf;
     recorrerHijos(sf, root, 1);
     */
-    procs_info_print();
+/*    procs_info_print();
 
     return 0;
 }
@@ -70,3 +70,51 @@ void cleanup_module(void)
 }
 
 MODULE_LICENSE("MIT");
+*/
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/sched/signal.h>
+#include <linux/sched.h>
+ 
+ 
+struct task_struct *task;        /*    Structure defined in sched.h for tasks/processes    */
+struct task_struct *task_child;        /*    Structure needed to iterate through task children    */
+struct list_head *list;            /*    Structure needed to iterate through the list in each task->children struct    */
+ 
+int iterate_init(void)                    /*    Init Module    */
+{
+    printk(KERN_INFO "%s","LOADING MODULE\n");    /*    good practice to log when loading/removing modules    */
+     
+    for_each_process( task ){            /*    for_each_process() MACRO for iterating through each task in the os located in linux\sched\signal.h    */
+        printk(KERN_INFO "\nPARENT PID: %d PROCESS: %s STATE: %ld",task->pid, task->comm, task->state);/*    log parent id/executable name/state    */
+        list_for_each(list, &task->children){                        /*    list_for_each MACRO to iterate through task->children    */
+ 
+            task_child = list_entry( list, struct task_struct, sibling );    /*    using list_entry to declare all vars in task_child struct    */
+     
+            printk(KERN_INFO "\nCHILD OF %s[%d] PID: %d PROCESS: %s STATE: %ld",task->comm, task->pid, /*    log child of and child pid/name/state    */
+                task_child->pid, task_child->comm, task_child->state);
+        }
+        printk("-----------------------------------------------------");    /*for aesthetics*/
+    }    
+     
+ 
+    return 0;
+ 
+}                /*    End of Init Module    */
+     
+void cleanup_exit(void)        /*    Exit Module    */
+{
+ 
+ 
+    printk(KERN_INFO "%s","REMOVING MODULE\n");
+ 
+}                /*    End of Exit Module    */
+ 
+module_init(iterate_init);    /*    Load Module MACRO    */
+module_exit(cleanup_exit);    /*    Remove Module MACRO    */
+ 
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("ITERATE THROUGH ALL PROCESSES/CHILD PROCESSES IN THE OS");
+MODULE_AUTHOR("Laerehte");
