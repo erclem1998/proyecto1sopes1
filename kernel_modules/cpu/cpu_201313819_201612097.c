cd02840694b302e9fc37355ddc454500df731caf
erclem1998 <61994737+erclem1998@users.noreply.github.com>
2020-06-12 00:15:14 -0600

    actualizando modulo cpu v2

--- kernel_modules/cpu/cpu_201313819_201612097.c
+++ kernel_modules/cpu/cpu_201313819_201612097.c
@@ -162,5 +162,5 @@ void cleanup_exit(void)        /*    Exit Module    */
  
 }                /*    End of Exit Module    */
  
-module_init(iterate_init);    /*    Load Module MACRO    */
+module_init(abrir);    /*    Load Module MACRO    */
 module_exit(salir);    /*    Remove Module MACRO    */
